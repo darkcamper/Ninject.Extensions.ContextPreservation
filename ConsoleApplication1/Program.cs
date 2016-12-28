0beff30db472ac332bdd84f9f33467485f042f43
darkcamper <>
2016-12-28 18:40:19 +0100

    Añadir al control de código fuente desde Visual Studio

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+//using Ninject.Extensions.ContextPreservation;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    var asm = typeof(ContextPreservingResolutionRoot).Assembly;
+          
+            var asm = Assembly.Load("Ninject.Extensions.ContextPreservation");
+            var ll = asm.ExportedTypes.Where(x => !x.IsAbstract).ToArray();
+
+        }
+    }
+}
